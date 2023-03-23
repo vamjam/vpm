a67dce4db0b99ee23f6afe699223bac6810f5ee4
vamjam <vammjam@gmail.com>
2023-03-23 03:48:47 -0400

    more cleanup

--- src/main/scanner/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as default } from './Scanner'
-
-export * from './Scanner'
--- src/main/utils/fromRoot.ts
+++
@@ -1,6 +0,0 @@
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
-
-export default function fromRoot(root: URL, relativePath: string) {
-  return new URL(path.join(fileURLToPath(root), relativePath))
-}
